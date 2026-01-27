5e30b30f7e981c8ff2e8c069010e1973f26b7674
0xVanfer <vanfer@vanfer.tech>
2026-01-27 16:33:08 +0800

    fix: config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/tools/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
